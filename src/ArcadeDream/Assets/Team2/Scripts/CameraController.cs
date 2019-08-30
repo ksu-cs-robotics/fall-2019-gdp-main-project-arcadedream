ead54466141d850d389775327a1bb39ce8fd1653
jdotson97 <jdotso11@kent.edu>
2019-08-30 15:07:03 -0400

    WASD, Camera Follow, and Testing Environment
    Added a new scene that may act as a testing grounds for some of our scripts. Added WASD movement using Input Manager instead of hard coded keys (PlayerController.cs), Camera follow based on a reference to the player gameObject (CameraController.cs). I also added a material for the ground to make movement a little easier to visualize.

---
+++ src/ArcadeDream/Assets/Team2/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Makes the parent (Camera) gameobject follow another referenced gameobject with a given offset
+/// Author: Josh Dotson
+/// Version: 1
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] public GameObject PLAYER;
+    [SerializeField] public float SMOOTHING = 0.05f;
+
+    protected Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - PLAYER.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, PLAYER.transform.position + offset, SMOOTHING);
+    }
+}
---
+++ src/ArcadeDream/Assets/Team2/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Implements basics controls for 
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float WALKSPEED = 3.0f;
+    [SerializeField] public float JUMPSPEED = 5.0f;
+
+    protected Rigidbody rigidbody_m;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody_m = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var movement = new Vector3();
+        var jump = new Vector3();
+
+        movement.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        movement = movement.normalized * WALKSPEED * Time.deltaTime;
+        jump.Set(0f, Input.GetAxisRaw("Jump"), 0f);
+        
+        if (!movement.Equals(Vector3.zero))
+        {
+            Vector3 newPosition = transform.position + movement;
+            Vector3 newPositionVector = newPosition - transform.position;
+            newPositionVector.y = 0f;
+
+            var newRotation = Quaternion.LookRotation(newPositionVector, Vector3.up);
+            var interpolatedRotation = Quaternion.Slerp(newRotation, transform.rotation, Time.deltaTime * 30);
+
+            rigidbody_m.MovePosition(transform.position + movement);
+            rigidbody_m.MoveRotation(interpolatedRotation);
+        }
+
+        if (!jump.Equals(Vector3.zero))
+        {
+            if (rigidbody_m.velocity.y == 0)
+            {
+                rigidbody_m.AddRelativeForce(Vector3.up * JUMPSPEED, ForceMode.Impulse);
+            }          
+        }
+    }
+}
+
