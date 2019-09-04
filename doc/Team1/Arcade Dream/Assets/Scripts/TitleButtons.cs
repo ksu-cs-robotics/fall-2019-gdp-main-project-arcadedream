fe65a0268f5b4541031dfc5537799ffadca93feb
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2019-09-04 00:15:27 -0400

    Setting up the basic frame of the game

---
+++ doc/Team1/Arcade Dream/Assets/Scripts/TitleButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleButtons : MonoBehaviour
+{
+    public string levelToLoad = "Main";
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Openning Setting");
+        //
+        //make setting UI visable 
+        //
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+
+
+}
