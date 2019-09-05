4f155135a2b416785c2f54e49acbcc5b17e1a381
PJ-Leyden <pleyden@kent.edu>
2019-09-04 23:20:14 -0400

    Add network manager to team1 initial arcade

---
+++ src/ArcadeDream/Assets/Team1/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿//9/2/2019
+//Noah Lin
+//version 1
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
+}
