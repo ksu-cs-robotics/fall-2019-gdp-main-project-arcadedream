75cdf0083ee3fef05c9b3512de9839a2d05f3eee
jdotson97 <jdotso11@kent.edu>
2019-09-07 00:48:57 -0400

    Implmented sprinting mechanics in PlayerContoller, and InputManager
    Still needs to be tested thoughly, but appears to be working great.

--- src/ArcadeDream/Assets/Team2/Scripts/PlayerController.cs
+++ src/ArcadeDream/Assets/Team2/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// Implements basics controls for WASD, and jumping movements
@@ -8,18 +9,25 @@ using UnityEngine;
 /// Version: 1
 /// </summary>
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour //NetworkBehaviour
 {
     [SerializeField] public float WALKSPEED = 3.0f;
+    [SerializeField] public float RUNSPEED = 5.0f;
     [SerializeField] public float JUMPSPEED = 5.0f;
 
     protected Rigidbody rigidbody_m;
     protected List<Collider> colliders_m;
 
+    public PlayerController()
+    {
+        colliders_m = new List<Collider>();
+    }
+
+    #region Start, and Update Functions
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody_m = GetComponent<Rigidbody>();
+        rigidbody_m = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
@@ -28,10 +36,17 @@ public class PlayerController : MonoBehaviour
         var movement = new Vector3();
         var jump = new Vector3();
 
+        // Get raw input values
         movement.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        movement = movement.normalized * WALKSPEED * Time.deltaTime;
+        movement = movement.normalized * Time.deltaTime;
+
+        // Choose movement multiplier based on whether or not the sprint input is used
+        if (!Input.GetAxis("Sprint").Equals(0)) { movement *= RUNSPEED; }
+        else { movement *= WALKSPEED; }
+        
         jump.Set(0f, Input.GetAxisRaw("Jump"), 0f);
         
+        // Process user movement input...
         if (!movement.Equals(Vector3.zero))
         {
             Vector3 newPosition = transform.position + movement;
@@ -44,7 +59,8 @@ public class PlayerController : MonoBehaviour
             rigidbody_m.MovePosition(transform.position + movement);
             rigidbody_m.MoveRotation(interpolatedRotation);
         }
-
+        
+        // Process user jumping input...
         if (!jump.Equals(Vector3.zero))
         {
             if (rigidbody_m.velocity.y == 0)
@@ -53,6 +69,7 @@ public class PlayerController : MonoBehaviour
             }          
         }
 
+        // Basic environmental interact function
         if (!Input.GetAxis("Submit").Equals(0))
         {
             var game = colliders_m.Find((c) => c.gameObject.tag == "Game");
@@ -67,7 +84,9 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+    #endregion
 
+    // When object comes in contact with the player, add it to the list of nearby objects
     private void OnTriggerEnter(Collider other)
     {
         if (!colliders_m.Contains(other))
@@ -76,6 +95,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // When object leaves the proximity of the player, remove it from the list of nearby objects
     private void OnTriggerExit(Collider other)
     {
         colliders_m.Remove(other);
