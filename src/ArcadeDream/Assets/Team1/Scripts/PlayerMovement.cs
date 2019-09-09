358de564b8c8e7e258614da5411de737212359ea
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2019-09-09 01:22:18 -0400

    Fixed Merge

---
+++ src/ArcadeDream/Assets/Team1/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+
+    }
+}
