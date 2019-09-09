296c3761e9aaf918226231c9e32e6886ad39c7e8
jdotson97 <jdotso11@kent.edu>
2019-09-09 17:43:04 -0400

    Publishing sprinting mechanics in PlayerController and InputManager
    Had to merge this with a previous commit which was AWFUL btw. Anyhow, did a few local commits over the weekend for the sprinting mechanics and added them in this commit.

--- src/ArcadeDream/Assets/Team2/Scripts/PlayerController.cs
+++ src/ArcadeDream/Assets/Team2/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float WALKSPEED = 3.0f;
+    [SerializeField] public float RUNSPEED = 5.0f;
     [SerializeField] public float JUMPSPEED = 5.0f;
 
     protected Rigidbody rigidbody_m;
@@ -35,9 +36,15 @@ public class PlayerController : MonoBehaviour
         var jump = new Vector3();
 
         movement.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        movement = movement.normalized * WALKSPEED * Time.deltaTime;
+        movement = movement.normalized * Time.deltaTime;
+
+        // Choose movement multiplier based on whether or not the sprint input is used
+        if (!Input.GetAxis("Sprint").Equals(0)) { movement *= RUNSPEED; }
+        else { movement *= WALKSPEED; }
+
         jump.Set(0f, Input.GetAxisRaw("Jump"), 0f);
 
+        // Process user movement input...
         if (!movement.Equals(Vector3.zero))
         {
             Vector3 newPosition = transform.position + movement;
@@ -51,6 +58,7 @@ public class PlayerController : MonoBehaviour
             rigidbody_m.MoveRotation(interpolatedRotation);
         }
 
+        //Process jumping input
         if (!jump.Equals(Vector3.zero))
         {
             if (rigidbody_m.velocity.y == 0)
@@ -59,6 +67,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        // Basic environmental interact function
         if (!Input.GetAxis("Submit").Equals(0))
         {
             var game = colliders_m.Find((c) => c.gameObject.tag == "Game");
@@ -79,18 +88,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // When object comes in contact with the player, add it to the list of nearby objects
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "NPC")
         {
             talkFlag_m = true;
         }
+
+        /*if (!colliders_m.Contains(other))
+        {
+            colliders_m.Add(other);
+        }*/
     }
 
+    // When object leaves the proximity of the player, remove it from the list of nearby objects
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.tag == "NPC")
         talkFlag_m = false;
         DIALOGUECANVAS.SetActive(false);
+
+        // colliders_m.Remove(other);
     }
 }
