e600abc070e70bd11ec2219f15cd1e288bb79f46
PJ-Leyden <pleyden@kent.edu>
2019-09-21 14:30:48 -0400

    Add test multi clients

---
+++ src/ArcadeDream/Assets/Team4/Scripts/ServerChecker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerChecker : MonoBehaviour
+{
+    public GameObject NetworkManger_m;
+    private MyNetworkManager nm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = NetworkManger_m.GetComponent<MyNetworkManager>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Image image = gameObject.GetComponent<Image>();
+        if (gameObject.name == "ServerOn")
+        {
+            if (nm.serverOn)
+            {
+                image.color = Color.green;
+            }
+            else
+            {
+                image.color = Color.red;
+            }
+        }
+        else if(gameObject.name == "ClientOn")
+        {
+            if (nm.clientOn)
+            {
+                image.color = Color.green;
+            }
+            else
+            {
+                image.color = Color.red;
+            }
+        }
+        else if (gameObject.name == "ConnectInternalOn")
+        {
+            if (nm.connected)
+            {
+                image.color = Color.green;
+            }
+            else
+            {
+                image.color = Color.red;
+            }
+        }
+    }
+}
