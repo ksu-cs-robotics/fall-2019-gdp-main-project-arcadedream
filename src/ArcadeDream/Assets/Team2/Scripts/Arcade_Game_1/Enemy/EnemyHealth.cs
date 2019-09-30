c3f7eebcdbcfe7457e3575035e5d6905981a60e6
PJ-Leyden <pleyden@kent.edu>
2019-09-30 14:37:52 -0400

    Revert "adding sound"
    This reverts commit 625d765037ca5187f7a1948c923218ee8d3e52ca, reversing
    changes made to dd0a6ea1f6616c54337e092b1b24aca9d904f174.

--- src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_1/Enemy/EnemyHealth.cs
+++ src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_1/Enemy/EnemyHealth.cs
@@ -16,19 +16,13 @@ public class EnemyHealth : MonoBehaviour
     //The sound to play when the enemy dies.
     public AudioClip DEATHCLIP;
     // Amount of time before enemy object is destroyed.
-    //public float DEATHDURATION = 2f;
+    public float DEATHDURATION = 2f;
 
     // Whether the enemy is dead.
     private bool isDead_m;
     // The amount of health the enemy currently has.
     private int currentHealth_m;
-<<<<<<< HEAD
 
-=======
-    // Reference to the animator.
-    //private Animator animator_m;
-    // Reference to the audio source.
->>>>>>> ab5b2bee80dac0eddc97bdcb236851cf1c5a66df
     private AudioSource audioSource_m;
 
     /// <summary>
@@ -46,13 +40,8 @@ public class EnemyHealth : MonoBehaviour
         currentHealth_m = STARTINGHEALTH;
 
         // Get references.
-<<<<<<< HEAD
         audioSource_m = GetComponent<AudioSource>();
         hb = GetComponent<Collider>();
-=======
-        //animator_m = GetComponent<Animator>();
-        audioSource_m = GetComponent<AudioSource>(); 
->>>>>>> ab5b2bee80dac0eddc97bdcb236851cf1c5a66df
     }
 
     public void TakeDamage()
@@ -61,26 +50,16 @@ public class EnemyHealth : MonoBehaviour
         if (isDead_m)
             return;
 
-<<<<<<< HEAD
      //   audioSource_m.Play();
           currentHealth_m--;
-=======
-        
-        currentHealth_m--;
->>>>>>> ab5b2bee80dac0eddc97bdcb236851cf1c5a66df
 
         if (currentHealth_m <= 0)
-        {
-            audioSource_m.clip = DEATHCLIP;
-            audioSource_m.Play();
             Die();
-        }
     }
 
     void Die()
     {
         isDead_m = true;
-<<<<<<< HEAD
 
         // Set the audio source to the death clip and play it.
         audioSource_m.clip = DEATHCLIP;
@@ -89,15 +68,10 @@ public class EnemyHealth : MonoBehaviour
         model.enabled = false;
         hb.enabled = false;
         this.enabled = false;
-=======
-        // Trigger the death animation.
-        //animator_m.SetTrigger("Dead");
-        // Set the audio source to the death clip and play it.
->>>>>>> ab5b2bee80dac0eddc97bdcb236851cf1c5a66df
         
         // Increase the player's score by the enemy's score value.
         // TODO: Implement score manager.
         // After the given amount of time, destroy the enemy object.
-        Destroy(gameObject);
+        Destroy(gameObject, DEATHDURATION);
     }
 }
