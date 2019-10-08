d1ee3fef7244be703bac65bbfc1aa8aeb8c99960
jdotson97 <jdotso11@kent.edu>
2019-10-08 17:20:17 -0400

    Added ADDBConnection Skeleton, and Modified ItemMap
    Added a commented out version of ADDBConnection until I figure out how to modify the target framework to .NET 4.6.1, and add a reference to System.Data.SqlClient (even after adding it with the NuGET browser, I can't access it).
    Added a GetPermission Function to ItemMap that returns whether or not based on the users permission database hash there aloud to equip an item, or display it as unlocked in the shop.

---
+++ src/ArcadeDream/Assets/Team2/Scripts/ADDBConnection.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Web;
+using System.Data;
+// using System.Data.SqlClient;
+// using FastMember;
+using UnityEngine;
+
+/// <summary>
+/// Simplifies the connection to and from an sql database, as well as the queries thereof
+/// Josh Dotson
+/// Simplicity Software Design LLC.
+/// Notes: requires FastMember library
+///
+/// Example Usage:
+/// 
+/// // The idea is that each member in this class has to map to some column in a particular database table
+/// public class ADLeaderBoardEntry
+/// {
+///     // Each of these members are the same name of a column in the database table
+///     public string PlayerID { get; set; }
+///     public int Score { get; set; }
+///     ...
+/// }
+/// 
+/// ...
+/// using (ADDBConnection connect = new ADDBConnection())
+/// {
+///     try
+///     {
+///         connect.Open()
+///         var leaderBoard = 
+///             connect.Query<ADLeaderBoardEntry>($"SELECT * FROM {(Table Name)}");
+///         ...
+///         
+///         // leaderBoard will now be a List<ADLeaderBoardEntry>, each element corrensponding to a tuple in the table
+///         FirstPlace = leaderBoard[0].Score
+///         SecondPlace = leaderBoard[1].Score
+///         ...
+///         
+///         connect.Query($"INSERT INTO {(Table Name)} VALUES ('{Player.PlayerID}', {Score})");
+///         ...
+///     }
+///     catch (Exception ex)
+///     {
+///         throw ex;
+///     }
+///     finally 
+///     {
+///         connect.Close();
+///     }
+/// }
+/// </summary>
+
+/*
+public class ADDBConnection : IDisposable
+{
+    private SqlConnection _sqlConnect;
+
+    public bool IsOpen { get; set; }
+    public bool IsElevated { get; }
+
+    public bool Failed { get; set; }
+    public string ErrorMessage { get; set; }
+
+    // Insert your connection string here
+    public static string ConnectionString = "";
+
+    public ADDBConnection()
+    {
+        _sqlConnect = new SqlConnection();
+
+        IsOpen = false;
+
+        Failed = false;
+        ErrorMessage = string.Empty;
+    }
+    public ADDBConnection(string newConnectionString) : this()
+    {
+        // If you'd wish to make the connectionString non static
+        // ConnectionString = newConnectionString;
+    }
+
+    // Used for write queries...
+    public void Query(string queryString)
+    {
+        try
+        {
+            var _command = _sqlConnect.CreateCommand();
+            _command.CommandText = queryString;
+            var _reader = _command.ExecuteReader();
+
+            _reader.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    // Used for read queries...
+    public List<T> Query<T>(string queryString) where T : new()
+    {
+        List<T> result = new List<T>();
+
+        try
+        {
+            var _command = _sqlConnect.CreateCommand();
+            _command.CommandText = queryString;
+            var _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
+            {
+                Type type = typeof(T);
+
+                // Create FastMember type accessor for field to member mapping
+                var accessor = TypeAccessor.Create(type);
+                // Stores a list of all member names within a class
+                var typeMembers = accessor.GetMembers();
+                // Create a new instance of T to store a tuple into
+                var tuple = new T();
+
+                for (int i = 0; i < _reader.FieldCount; i++)
+                {
+                    if (!_reader.IsDBNull(i))
+                    {
+                        // Get the name of a db field in the table...
+                        string fieldName = _reader.GetName(i);
+
+                        // Look for any members inside T with the same name, ignoring the case...
+                        if (typeMembers.Any(m => string.Equals(m.Name, fieldName, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            // If there is a member in T that maps to a field column, use the FastMember accessor to map it into tuple
+                            accessor[tuple, fieldName] = _reader.GetValue(i);
+                        }
+                    }
+                }
+
+                // Add this tuple into the full result, and move on
+                result.Add(tuple);
+            }
+
+            _reader.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return result;
+    }
+
+    public void Open()
+    {
+        _sqlConnect = new SqlConnection(ConnectionString);
+
+        try
+        {
+            _sqlConnect.Open();
+            IsOpen = true;
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.ToString();
+            IsOpen = false;
+
+            throw ex;
+        }
+    }
+    public void Close()
+    {
+        _sqlConnect.Close();
+        _sqlConnect.Dispose();
+    }
+    public void Dispose()
+    {
+        _sqlConnect.Dispose();
+    }
+} */
+
--- src/ArcadeDream/Assets/Team2/Scripts/ItemMap.cs
+++ src/ArcadeDream/Assets/Team2/Scripts/ItemMap.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Stores an Item gameObject along with its corresponding ID
+/// Author: Josh Dotson
+/// Version: 1
+/// </summary>
 public class Item
 {
     public int ID;
@@ -18,25 +24,67 @@ public class Item
     }
 }
 
+/// <summary>
+/// Maps database ids to actual gameObjects in game. Useful for equipable items.
+/// Author: Josh Dotson
+/// Version: 1
+/// Notes: Inherits from MonoBehaviour to allow for more predictable behaviour in game...
+/// </summary>
 public class ItemMap : MonoBehaviour
 {
-    public List<Item> idMap;
+    public List<Item> IDMap;
+    private static readonly uint MapSize = 32;
 
     // Can't comma these to make them neater, as the editor doesn't like that
     [TextArea] public string Description1;
-    [SerializeField] GameObject id0, id1, id2, id3, id4, id5, id6, id7;
+    [SerializeField] GameObject ID0, ID1, ID2, ID3, ID4, ID5, ID6, ID7;
 
     [TextArea] public string Description2;
-    [SerializeField] GameObject id8, id9, id10, id11, id12, id13, id14, id15;
+    [SerializeField] GameObject ID8, ID9, ID10, ID11, ID12, ID13, ID14, ID15;
 
     [TextArea] public string Description3;
-    [SerializeField] GameObject id16, id17, id18, id19, id20, id21, id22, id23;
+    [SerializeField] GameObject ID16, ID17, ID18, ID19, ID20, ID21, ID22, ID23;
 
     [TextArea] public string Description4;
-    [SerializeField] GameObject id24, id25,id26,id27,id28,id29,id30,id31;
+    [SerializeField] GameObject ID24, ID25, ID26, ID27, ID28, ID29, ID30, iID31;
 
-    public void Start()
+    /// <summary>
+    /// Takes in both a full db hash, and an item ID, and returns true if
+    /// the item is unlocked/unrestricted
+    /// </summary>
+    /// <param name="permissionsHash">Unsigned 32 Bit int from Database</param>
+    /// <param name="itemID">ID of the Item in Question</param>
+    /// <returns>Whether or not an item is unlocked</returns>
+    public static bool GetPermission(uint permissionsHash, uint itemID)
     {
-        idMap = new List<Item>();
+        // Logical AND the permissionsHash with 2^ID, and convert that result to a bool (because C#)
+        return Convert.ToBoolean(permissionsHash & ((uint)Math.Pow(2, itemID)));
+    }
+
+    /// <summary>
+    /// Takes an equipment hash from the database, and returns a list of equipped gameObjects based on it
+    /// </summary>
+    /// <param name="equipmentHash">Unsigned 32 Bit int from Database</param>
+    /// <returns>List of gameObjects the Player Should Be Wearing</returns>
+    public static List<GameObject> GetEquippedItems(uint equipmentHash)
+    {
+        // ...
+        
+        return new List<GameObject>(); // Placeholder
+    }
+
+    public void Awake()
+    {
+        // Initialize the map list
+        IDMap = new List<Item>();
+
+        // This uses reflection to avoid typing out all of the variable names
+        for (var i = 0; i <= MapSize - 1; ++i)
+        {
+            Item item = new Item(i); // Create a new Item with an ID
+            item.item = (GameObject)typeof(ItemMap).GetField($"ID{i}").GetValue(this); // Using reflection, set the item reference
+
+            IDMap.Add(item);
+        }
     }
 }
