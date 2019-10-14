36c4ea0de4f007c050a97bf328221c9122f9cf56
etaylo37 <47994067+etaylo37@users.noreply.github.com>
2019-10-14 14:29:20 -0400

    Goalie Created
    Goalie prefab and script

---
+++ src/ArcadeDream/Assets/Team1/Subgame2/Scripts/GoalieMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GoalieMovement : MonoBehaviour
+{
+    public int leftlimit;
+    public int rightlimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int leftlimit = 0;
+        int rightlimit = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A)&&leftlimit!=1)
+        {
+            this.gameObject.transform.Translate(-1, 0, 0);
+            leftlimit++;
+            rightlimit--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)&&rightlimit!=1)
+        {
+            this.gameObject.transform.Translate(1, 0, 0);
+            leftlimit--;
+            rightlimit++;
+        }
+    }
+}
