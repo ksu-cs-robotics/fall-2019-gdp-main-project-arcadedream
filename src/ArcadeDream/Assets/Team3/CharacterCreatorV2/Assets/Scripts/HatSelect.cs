6a3a1cd34c4ab033f5c857019d4940c9e5c1923a
JRTrussell <54453474+JRTrussell@users.noreply.github.com>
2019-10-28 13:27:45 -0400

    Added the finished version of charactercreator pre UI

---
+++ src/ArcadeDream/Assets/Team3/CharacterCreatorV2/Assets/Scripts/HatSelect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatSelect : MonoBehaviour
+{
+    public GameObject hatarea;
+    public GameObject[] hats;
+    private GameObject hat;
+    int hatcount = 0;
+    void Start()
+    {
+      
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("j")) //increment shirt count and save to loadout
+        {
+            Destroy(hat);
+            hatcount++;
+            if (hatcount >= hats.Length)
+            {
+                hatcount = 0;
+            }
+            hat = Instantiate(hats[hatcount]);
+            hat.transform.SetParent(hatarea.transform);
+        }
+
+        if (Input.GetKeyDown("k")) //increment shirt count and save to loadout
+        {
+            Destroy(hat);
+            hatcount--;
+            if (hatcount < 0)
+            {
+                hatcount = hats.Length - 1;
+            }
+            hat = Instantiate(hats[hatcount]);
+            hat.transform.SetParent(hatarea.transform);
+        }
+    }
+}
--- src/ArcadeDream/Assets/Team3/CharacterCreatorV2/Assets/Scripts/MatirialSelector.cs
+++ src/ArcadeDream/Assets/Team3/CharacterCreatorV2/Assets/Scripts/MatirialSelector.cs
@@ -9,17 +9,22 @@ public class MatirialSelector : MonoBehaviour
     public GameObject player;
     public Material skins; //skin color array (changed inunity editor)
     public Material[] shirts; //shirt color array (changed in unity editor)
-    private Material[] loadout = new Material[2]; //loadout of custom items
+    public Material[] eyes; //eye array
+    private Material[] loadout = new Material[3]; //loadout of custom items
     private SkinnedMeshRenderer MeshSkin; //element grabbing skinned mesh renderer
     private float red, blue, green; //floats for skin color
+    private Animator anim;
     public Slider sred, sblue, sgreen; //sliders for skin color
     public int skincount = 0;
     public int shirtcount = -1;
-    // Create some asset folders.
+    public int eyecount = -1;
+    public float timeLeft;
     string prefabPath = "Assets/Prefabs/Player01.prefab";
-    // Start is called before the first frame update
+
+   
     void Start()
     {
+        anim = transform.parent.gameObject.GetComponent<Animator> ();
         MeshSkin = GetComponent<SkinnedMeshRenderer>(); //testing
         loadout[0] = skins; //set the skin material
 
@@ -32,21 +37,31 @@ public class MatirialSelector : MonoBehaviour
         blue = sblue.value;
         green = sgreen.value;
         skins.SetColor("_Color", new Color(red, green, blue, 0f)); //set the skin color
+        MeshSkin.materials = loadout;
 
+        ///////////////////////////////////////Shirts/////////////////////////////////////////////////////////////////////////////////////////////////
         if (Input.GetKeyDown("d")) //increment shirt count and save to loadout
         {
             shirtcount++;
+            anim.SetBool("PlayCheckOut", false);
+            anim.SetBool("PlayCheckOut", true);
+            timeLeft = 1f;
             if (shirtcount >= shirts.Length)
             {
                 shirtcount = 0;
             }
             loadout[1] = shirts[shirtcount];
             MeshSkin.materials = loadout;
+
+            //anim.SetBool("PlayCheckout", false);
         }
 
         if (Input.GetKeyDown("a")) //increment shirt count and save to loadout
         {
             shirtcount--;
+            anim.SetBool("PlayCheckOut", false);
+            anim.SetBool("PlayCheckOut", true);
+            timeLeft = 1f;
             if (shirtcount < 0)
             {
                 shirtcount = shirts.Length - 1;
@@ -55,9 +70,46 @@ public class MatirialSelector : MonoBehaviour
             MeshSkin.materials = loadout;
         }
 
+
+        if (Input.GetKeyDown("w")) //increment shirt count and save to loadout
+        {
+            eyecount++;
+            if (eyecount >= eyes.Length)
+            {
+                eyecount = 0;
+            }
+            loadout[2] = eyes[eyecount];
+            MeshSkin.materials = loadout;
+
+            //anim.SetBool("PlayCheckout", false);
+        }
+
+        if (Input.GetKeyDown("s")) //increment shirt count and save to loadout
+        {
+            eyecount--;
+            timeLeft = 1f;
+            if (eyecount < 0)
+            {
+                eyecount = eyes.Length - 1;
+            }
+            loadout[2] =eyes[eyecount];
+            MeshSkin.materials = loadout;
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
         if (Input.GetKeyDown("l")) //save player object as prefab for saving
         {
             PrefabUtility.SaveAsPrefabAsset(player, prefabPath);
+        }
+
+        if (timeLeft > -1)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                anim.SetBool("PlayCheckOut", false);
+            }
         }
     }
 }
