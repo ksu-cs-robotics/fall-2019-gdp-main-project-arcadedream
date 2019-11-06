7d6db76eb634d5c976da5014c5a1c6279d118e06
Tyler Wells <twells14@kent.edu>
2019-11-06 14:44:58 -0500

    update

--- src/ArcadeDream/Assets/MainCamera.cs
+++ src/ArcadeDream/Assets/MainCamera.cs
@@ -20,27 +20,9 @@ public class MainCamera : MonoBehaviour
 
     void Update()
     {
-        //if (movingHoriz)
-       // {
-            targetCamPos = new Vector2(player.transform.position.x, transform.position.y);
+            targetCamPos = new Vector2(player.transform.position.x, player.transform.position.y);
             transform.position = Vector2.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
-       // }
-        //if (movingVert)
-       // {
-            targetCamPos = new Vector2(transform.position.x, player.transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-            transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
-        //}
-
-        /*
-        if (player.transform.position.x - transform.position.x >= horizMovePoint || player.transform.position.x - transform.position.x <= -horizMovePoint)
-            movingHoriz = true;
-        else movingHoriz = false;
-
-        if (player.transform.position.y - transform.position.y >= vertMovePoint || player.transform.position.y - transform.position.y <= -vertMovePoint)
-            movingVert = true;
-        else movingVert = false;
-        */
+      
     }
 }
