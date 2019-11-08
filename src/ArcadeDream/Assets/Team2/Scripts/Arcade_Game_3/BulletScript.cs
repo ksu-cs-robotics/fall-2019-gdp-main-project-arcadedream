60ad8134d53291f3e21092f9720205d3fca79945
LewFortwangler <lfortwan@kent.edu>
2019-11-07 20:18:46 -0500

    Started networking bullets

---
+++ src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_3/BulletScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletScript : NetworkBehaviour
+{
+    private Rigidbody2D rb;
+    public float bulletSpeed;
+
+    public float despawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(despawn());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Ground")
+        NetworkServer.Destroy(gameObject);
+    }
+
+    IEnumerator despawn()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        NetworkServer.Destroy(gameObject);
+    }
+}
---
+++ src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_3/GUN.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class GUN : NetworkBehaviour
+{
+    public GameObject playerPos;
+    public GameObject bullet;
+    public static float angle;
+    public GameObject bow_;
+    Vector3 mousePos;
+
+    public float fireRate;
+    private float timeLeft = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bow_move();
+
+        if (Input.GetKeyDown(KeyCode.E) && timeLeft <= 0) 
+        {
+            Instantiate(bullet, transform.position, playerPos.transform.rotation);
+            NetworkServer.Spawn(bullet);
+            timeLeft = fireRate;
+        }
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        
+    }
+
+    private void bow_move() //this code was taken from https://answers.unity.com/questions/1326563/top-down-2d-game-aim-weapon-with-mouse-please-help.html
+    {
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - playerPos.transform.position;
+        angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        playerPos.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
