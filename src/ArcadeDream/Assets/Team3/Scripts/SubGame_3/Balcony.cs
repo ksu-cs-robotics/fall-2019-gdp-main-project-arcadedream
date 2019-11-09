c2b53fa6309f1aaacbebe35941749b934b79ebce
JTRUSSE1 <jtrusse1@kent.edu>
2019-11-08 22:58:22 -0500

    Base Balcony Done

---
+++ src/ArcadeDream/Assets/Team3/Scripts/SubGame_3/Balcony.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balcony : MonoBehaviour
+{
+
+    public GameObject Rubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nothin for now
+    }
+
+
+    void Update()
+    {
+        //nothin for now
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Bullet")
+        {
+            Rubble.GetComponent<SpriteRenderer>().enabled = true;
+            Rubble.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
