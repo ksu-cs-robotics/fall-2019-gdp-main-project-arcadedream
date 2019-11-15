907c7aa72d2685489515f8ba475c8ec866e011e2
PJ-Leyden <pleyden@kent.edu>
2019-11-15 18:20:43 -0500

    LocalPlayer stuff

--- src/ArcadeDream/Assets/Team3/Scripts/SubGame_3/GUN.cs
+++ src/ArcadeDream/Assets/Team3/Scripts/SubGame_3/GUN.cs
@@ -23,7 +23,7 @@ public class GUN : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire1")) && timeLeft <= 0 && canShoot) 
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire1")) && timeLeft <= 0 && canShoot && isLocalPlayer) 
         {
             mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
             Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
