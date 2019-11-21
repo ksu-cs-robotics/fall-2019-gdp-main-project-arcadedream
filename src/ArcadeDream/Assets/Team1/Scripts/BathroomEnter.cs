30718559fb1a98574508360f68b35a1c13fe70ef
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2019-11-21 00:41:23 -0500

    Finished new arcade design
    now with bathroom

---
+++ src/ArcadeDream/Assets/Team1/Scripts/BathroomEnter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathroomEnter : MonoBehaviour
+{
+    public GameObject[] walls;
+    Animator wallAnim;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("disappear");
+            foreach (GameObject wall in walls)
+            {
+                wallAnim = wall.GetComponent<Animator>();
+                wallAnim.Play("WallDisappear");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("reappear");
+            foreach (GameObject wall in walls)
+            {
+                wallAnim = wall.GetComponent<Animator>();
+                wallAnim.Play("WallAppear");
+            }
+        }
+    }
+}
---
+++ src/ArcadeDream/Assets/Team1/Scripts/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animator door;
+
+    private void Start()
+    {
+        door = GetComponent<Animator>();
+        //door.Play("DoorClose");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("door open");
+            door.Play("DoorOpen");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("door close");
+            door.Play("DoorClose");
+        }
+    }
+
+}
