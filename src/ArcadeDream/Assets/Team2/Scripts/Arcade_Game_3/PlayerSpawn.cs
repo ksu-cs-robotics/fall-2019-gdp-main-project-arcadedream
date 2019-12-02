299f9f0f744734c52d392f3fde78fbfc7264dd45
Lew Fortwangler <lfortwan@kent.edu>
2019-12-02 16:37:49 -0500

    work on bloodrun photon networking

--- src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_3/PlayerSpawn.cs
+++ src/ArcadeDream/Assets/Team2/Scripts/Arcade_Game_3/PlayerSpawn.cs
@@ -5,75 +5,79 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlayerSpawn : MonoBehaviourPunCallbacks
+namespace Bloodrun
 {
-    public GameObject player;
-    public GameObject spawnPoint;
-    public GameObject spawnPoint2;
-    public GameObject spawnPoint3;
-    public GameObject spawnPoint4;
-
-    public Text countdown;
-
-    private int minPlayers = 2;
-    private int maxPlayers = 4;
-    private int currentPlayers = 0;
-    private int seconds = 5;
-
-    private bool gameStarted = false;
-
-    void Start()
+    public class PlayerSpawn : MonoBehaviourPunCallbacks
     {
-        countdown.text = "5";
-        if(currentPlayers == 0)
-        {
-            PhotonNetwork.Instantiate(player.name, spawnPoint.transform.position, Quaternion.identity, 0);
-            ++currentPlayers;
-        }
-    }
+        [SerializeField]
+        public GameObject player;
 
-    void Update()
-    {
-        if(gameStarted == false)
+        public GameObject spawnPoint;
+        public GameObject spawnPoint2;
+        public GameObject spawnPoint3;
+        public GameObject spawnPoint4;
+        public GameObject button;
+
+        public Text countdown;
+
+        private int minPlayers = 1;
+        private int maxPlayers = 4;
+        private int seconds = 5;
+        [SerializeField]
+        private int currentPlayers;
+
+        private bool gameStarted = false;
+
+        void Start()
         {
-            SpawnPlayers();
+            currentPlayers = PhotonNetwork.PlayerList.Length;
+            button.SetActive(false);
+            countdown.text = "5";
+            if (currentPlayers == 0)
+            {
+                PhotonNetwork.Instantiate(player.name, spawnPoint.transform.position, Quaternion.identity, 0);
+            }
+            if (currentPlayers == 1)
+            {
+                PhotonNetwork.Instantiate(player.name, spawnPoint2.transform.position, Quaternion.identity, 0);
+            }
+            if (currentPlayers == 2)
+            {
+                PhotonNetwork.Instantiate(player.name, spawnPoint3.transform.position, Quaternion.identity, 0);
+            }
+            if (currentPlayers == 3)
+            {
+                PhotonNetwork.Instantiate(player.name, spawnPoint4.transform.position, Quaternion.identity, 0);
+            }
         }
 
-        if(currentPlayers >= minPlayers)
+        void Update()
         {
-            //activate start button, button starts countDown
+            if (currentPlayers >= minPlayers)
+            {
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    button.SetActive(true);
+                }
+            }
+            if (currentPlayers == maxPlayers)
+            {
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+                PhotonNetwork.CurrentRoom.IsVisible = false;
+            }
+        }
+
+        public void Wrapper()
+        {
             gameStarted = true;
+            StartCoroutine("Countdown");
         }
-        if(currentPlayers == maxPlayers)
-        {
-            PhotonNetwork.CurrentRoom.IsOpen = false;
-            PhotonNetwork.CurrentRoom.IsVisible = false;
-        }
-    }
-    
-    private void SpawnPlayers()
-    {
-        if (currentPlayers == 1)
-        {
-            PhotonNetwork.Instantiate(player.name, spawnPoint2.transform.position, Quaternion.identity, 0);
-            ++currentPlayers;
-        }
-        if (currentPlayers == 2)
-        {
-            PhotonNetwork.Instantiate(player.name, spawnPoint3.transform.position, Quaternion.identity, 0);
-            ++currentPlayers;
-        }
-        if (currentPlayers == 3)
-        {
-            PhotonNetwork.Instantiate(player.name, spawnPoint4.transform.position, Quaternion.identity, 0);
-            ++currentPlayers;
-        }
-    }
 
-    private IEnumerable CountDown()
-    {
-        yield return new WaitForSeconds(1);
-        countdown.text = $"{seconds}";
-        --seconds;
+        private IEnumerator CountDown()
+        {
+            yield return new WaitForSeconds(1);
+            countdown.text = $"{seconds}";
+            --seconds;
+        }
     }
 }
